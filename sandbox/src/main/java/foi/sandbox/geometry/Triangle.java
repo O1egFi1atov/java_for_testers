20e5717eec5047cd6c78e63d6ddd8a53cff5fdbe
O1egFi1atov <filatov4business@gmail.com>
2024-07-01 23:52:16 +0300

    Вместо нотации record вернул class. Применение нотации record закомментировал в Triangle.

--- sandbox/src/main/java/foi/sandbox/geometry/Triangle.java
+++ sandbox/src/main/java/foi/sandbox/geometry/Triangle.java
@@ -1,25 +1,16 @@
 package foi.sandbox.geometry;
 
-public record Triangle(double a, double b, double c) {
-//    свойства класса и конструктор объекта ниже закомментировал, поскольку вместо class применил нотацию record (Лекция 1.9.)
-//    private final double a;
-//    private final double b;
-//    private final double c;
-//
-//    public Triangle(double a, double b, double c){
-//        this.a = a;
-//        this.b = b;
-//        this.c = c;
-//    }
+public class Triangle {
+    // свойства класса:
+    private final double a;
+    private final double b;
+    private final double c;
 
-    // Применил ветвление (Лекция 2.3), для этого требуется создать конструктор. Обрати внимание, поскольку ранее Triangle создан как record, поэтому перед конструктором не указаны свойства объекта (стороны треугольника), а у конструктора в параметрах не указаны значения с типами этих значений)
-    public Triangle {
-        if (a <= 0 || b <= 0 || c <= 0) {
-            throw new IllegalArgumentException("Triangle side cannot be negative.");
-        }
-        if (a + b <= c || a + c <= b || b + c <= a) {
-            throw new IllegalArgumentException("The sum of any two sides must not be less than the third side.");
-        }
+    // конструктор объекта:
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
     // метод вычисления периметра
@@ -32,5 +23,45 @@ public record Triangle(double a, double b, double c) {
         var hp = perimeter() / 2;
         return Math.sqrt(hp * (hp - this.a) * (hp - this.b) * (hp - this.c));
     }
-
 }
+
+/* Ниже код, решающий ту же самую задачу, но более современным способом, а именно пример использования нотации record вместо class, стало доступно в версиях JAVA 17 и новее */
+//public record Triangle(double a, double b, double c) {
+//    свойства класса и конструктор объекта ниже закомментировал, поскольку вместо class применил нотацию record (Лекция 1.9. курса JAVA 17)
+//
+/* свойства класса: */
+//    private final double a;
+//    private final double b;
+//    private final double c;
+//
+/* конструктор объекта: */
+//    public Triangle(double a, double b, double c){
+//        this.a = a;
+//        this.b = b;
+//        this.c = c;
+//    }
+
+/* Применил ветвление (Лекция 2.3 курса JAVA 17), для этого требуется создать конструктор.
+Обрати внимание, поскольку ранее Triangle создан как record, поэтому перед конструктором не указаны свойства объекта (стороны треугольника),
+а у конструктора в параметрах не указаны значения с типами этих значений) */
+//    public Triangle {
+//        if (a <= 0 || b <= 0 || c <= 0) {
+//            throw new IllegalArgumentException("Triangle side cannot be negative.");
+//        }
+//        if (a + b <= c || a + c <= b || b + c <= a) {
+//            throw new IllegalArgumentException("The sum of any two sides must not be less than the third side.");
+//        }
+//    }
+//
+//    // метод вычисления периметра
+//    public double perimeter() {
+//        return this.a + this.b + this.c;
+//    }
+//
+//    // метод вычисления площади
+//    public double area() {
+//        var hp = perimeter() / 2;
+//        return Math.sqrt(hp * (hp - this.a) * (hp - this.b) * (hp - this.c));
+//    }
+//
+//}
