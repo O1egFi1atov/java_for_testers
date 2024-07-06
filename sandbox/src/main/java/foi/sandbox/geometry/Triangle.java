bb4f83cb22aa455258ed77c625d2ad397b3a45c1
O1egFi1atov <filatov4business@gmail.com>
2024-07-06 21:08:17 +0300

    Выполнено домашнее задание первого модуля.

--- sandbox/src/main/java/foi/sandbox/geometry/Triangle.java
+++ sandbox/src/main/java/foi/sandbox/geometry/Triangle.java
@@ -1,16 +1,18 @@
 package foi.sandbox.geometry;
 
+// класс Triangle содержит структуру объектов, содержащую информацию о треугольниках
 public class Triangle {
 
-//    описываем структуру класса (Лекция 1.8. Классы и объекты JAVA17):
-
-    // свойства класса (у объектов этого класса будет три значения типа число с плавающей точкой,
-    // каждое из которых будет определять сторону треугольника; также определяем модификатор видимости private):
+    // 1. Описываем структуру объекта.
+// описание структуры объекта перечисляет его свойства:
+// у объектов этого класса будет три свойства типа число с плавающей точкой, каждое из которых соответствует стороне
+// треугольника; также определяем модификатор видимости private):
+// подробно в Лекция 1.8. Классы и объекты JAVA17 и Лекция 1.9. Record особый вид классов JAVA17
     private final double sideA;
     private final double sideB;
     private final double sideC;
 
-    // конструктор объекта (специальная функция, которая инициализирует объекты этого класса)
+    // 2. Конструктор объекта (специальная функция, которая инициализирует объекты этого класса)
     // тут мы присваеваем свойствам объекта (то,что выше) конкретные значения (то, что в скобочках), используя ключевое слово this
     public Triangle(double sideA, double sideB, double sideC) {
         this.sideA = sideA;
@@ -29,46 +31,33 @@ public class Triangle {
         var halfPerimeter = perimeter() / 2;
         return Math.sqrt(halfPerimeter * (halfPerimeter - this.sideA) * (halfPerimeter - this.sideB) * (halfPerimeter - this.sideC));
     }
-}
 
-/* Ниже альтернативный способ описания структуры объектов, а именно пример использования нотации record вместо class, стало доступно в версиях JAVA 17 и новее */
-//
-//public record Triangle(double a, double b, double c) {
-//    свойства класса и конструктор объекта ниже закомментировал, поскольку вместо class применил нотацию record (Лекция 1.9. курса JAVA 17)
-//
-/* свойства класса: */
-//    private final double a;
-//    private final double b;
-//    private final double c;
-//
-/* конструктор объекта: */
-//    public Triangle(double a, double b, double c){
-//        this.a = a;
-//        this.b = b;
-//        this.c = c;
-//    }
-
-/* Применил ветвление (Лекция 2.3 курса JAVA 17), для этого требуется создать конструктор.
-Обрати внимание, поскольку ранее Triangle создан как record, поэтому перед конструктором не указаны свойства объекта (стороны треугольника),
-а у конструктора в параметрах не указаны значения с типами этих значений) */
+    /* Применяем ветвление (Лекция 2.3 курса JAVA 17), для этого требуется создать конструктор.
+    Обрати внимание, поскольку ранее Triangle создан как record, поэтому перед конструктором не указаны свойства объекта (стороны треугольника),
+    а у конструктора в параметрах не указаны значения с типами этих значений) */
 //    public Triangle {
-//        if (a <= 0 || b <= 0 || c <= 0) {
+//        if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
 //            throw new IllegalArgumentException("Triangle side cannot be negative.");
 //        }
-//        if (a + b <= c || a + c <= b || b + c <= a) {
+//        if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA) {
 //            throw new IllegalArgumentException("The sum of any two sides must not be less than the third side.");
 //        }
 //    }
+}
+
+    /* Ниже альтернативный способ описания структуры объектов, то есть описания классов. А именно,
+пример использования нотации record вместо class, стало доступно в версиях JAVA 14 и новее.
+(подробно в Лекция 1.9. курса JAVA 17) */
 //
-//    // метод вычисления периметра
+//public record Triangle(double a, double b, double c) {
 //    public double perimeter() {
-//        return this.a + this.b + this.c;
+//        return this.sideA + this.sideB + this.sideC;
 //    }
-//
-//    // метод вычисления площади
-//    public double area() {
-//        var hp = perimeter() / 2;
-//        return Math.sqrt(hp * (hp - this.a) * (hp - this.b) * (hp - this.c));
+//     public double area() {
+//        var halfPerimeter = perimeter() / 2;
+//        return Math.sqrt(halfPerimeter * (halfPerimeter - this.sideA) * (halfPerimeter - this.sideB) * (halfPerimeter - this.sideC));
 //    }
-//
-//}
+//    }
+// поскольку вместо class применил нотацию record, в которой свойства объекта можно прописать в круглых скобках, то
+// отдельно прописывать свойства класса и создавать конструктор объекта не требуется, так как вся информация уже
+// заключена в описании класса. В этом преимущество нотации record.
